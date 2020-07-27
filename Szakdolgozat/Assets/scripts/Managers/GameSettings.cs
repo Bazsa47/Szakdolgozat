5cfe9b7c1de6ce30c3c3130a91a08ef1bb6e05a3
Bazsa47 <55391083+Bazsa47@users.noreply.github.com>
2020-07-27 12:31:47 +0200

    Setting up server connection
    Preparing connection to server

---
+++ Szakdolgozat/Assets/scripts/Managers/GameSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Manager/GameSettings")]
+public class GameSettings : SingletonScriptableObject<MasterManager>
+{
+    [SerializeField]
+    private string gameVersion = "0.0.0";
+    [SerializeField]
+    private string nickName = "Default";
+
+    public string GameVersion { get => gameVersion;  }
+    public string NickName { get => nickName; }
+}
