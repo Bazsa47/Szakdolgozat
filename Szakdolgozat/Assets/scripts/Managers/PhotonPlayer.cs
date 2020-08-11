a4a3333c6c56f8e244fdd9744156c7bde5d8b048
Bazsa47 <55391083+Bazsa47@users.noreply.github.com>
2020-08-11 13:05:31 +0200

    Spawning players
    Upon entering a game, players are now spawned, and can see each other's movements

---
+++ Szakdolgozat/Assets/scripts/Managers/PhotonPlayer.cs
@@ -0,0 +1,22 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class PhotonPlayer : MonoBehaviour
+{
+    private PhotonView PV;
+    public GameObject myAvatar;
+
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        int spawnPoint = Random.Range(0,GameSetup.GS.spawnPoints.Length);
+        if (PV.IsMine)
+        {
+            myAvatar = PhotonNetwork.Instantiate("PlayerAvatar",
+                GameSetup.GS.spawnPoints[spawnPoint].position,GameSetup.GS.spawnPoints[spawnPoint].rotation,0);
+        }
+    }
+}
---
+++ Szakdolgozat/Assets/scripts/SpawnPlayers.cs
@@ -0,0 +1,15 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpawnPlayers : MonoBehaviour
+{
+
+    void Awake() 
+    {
+        PhotonNetwork.Instantiate("PhotonNetworkPlayer",transform.position,Quaternion.identity,0);
+    }
+}
