3b65c15e5907b4d72bb16dc4c2eb212ff1b3b716
Bazsa47 <balazs.wolf@gmail.hu>
2021-01-19 11:44:46 +0100

    dummy enemies
    implemented dummy enemies to check network broadcast.

---
+++ Szakdolgozat/Assets/scripts/testEnemySlain.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testEnemySlain : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.other.CompareTag("Player"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
