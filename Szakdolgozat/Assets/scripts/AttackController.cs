0ba774943285a909d30b52fe68e203b4a67b90a3
Bazsa47 <balazs.wolf@gmail.hu>
2021-02-06 18:50:38 +0100

    Added spear aniamtion and weapon controller
    players can now switch weapons with "Q", and the current weapon animation will play.

--- Szakdolgozat/Assets/scripts/AttackController.cs
+++ Szakdolgozat/Assets/scripts/AttackController.cs
@@ -5,31 +5,63 @@ using UnityEngine;
 
 public class AttackController : MonoBehaviour
 {
-    public Animation swordSwing;
-    public Collider swordCollider;
-    PhotonView PV;
+    public Animation[] anim;
+    public Collider[] weaponCollider;
+    public PhotonView PV;
+
+    private int currentWeaponIndex = 0;
+    public GameObject sword, spear;
+    private void FixedUpdate()
+    {
+        //switch weapon
+        Debug.Log(currentWeaponIndex);
+        if (Input.GetKeyDown(KeyCode.Q) && !anim[currentWeaponIndex].isPlaying)
+        {
+            currentWeaponIndex += 1;
+            if (currentWeaponIndex > 1)
+            {
+                currentWeaponIndex = 0;
+            }
+
+
+            switch ((Weapons)(currentWeaponIndex))
+            {
+                case Weapons.Sword:
+                    sword.SetActive(true);
+                    spear.SetActive(false);
+                    //ranged.SetActive(false);
+                    break;
+                case Weapons.Spear:
+                    sword.SetActive(false);
+                    spear.SetActive(true);
+                    //ranged.SetActive(false);
+                    break;
+                case Weapons.Ranged:
+                    sword.SetActive(false);
+                    spear.SetActive(false);
+                    //ranged.SetActive(true);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        //basic attack
+        Debug.Log(currentWeaponIndex);
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !anim[currentWeaponIndex].isPlaying)
+            {
+                anim[currentWeaponIndex].Play();
+                weaponCollider[currentWeaponIndex].enabled = true;
+            }
+            if (!anim[currentWeaponIndex].isPlaying)
+            {
+                weaponCollider[currentWeaponIndex].enabled = false;
+            }
+    }
+    public enum Weapons { Sword = 0, Spear = 1, Ranged = 2 };
     void Start()
     {
         PV = GetComponent<PhotonView>();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (PV.IsMine)
-        {
-
-            if (Input.GetKeyDown(KeyCode.Mouse0) && !swordSwing.isPlaying)
-            {
-                swordSwing.Play();
-                swordCollider.enabled = true;
-            }
-
-            if (!swordSwing.isPlaying)
-            {
-                swordCollider.enabled = false;
-            }
-        }
-       
-    }
 }
