01cc2a02422e0f23315f74c3c3c750972682407a
Bazsa47 <balazs.wolf@gmail.hu>
2021-02-09 16:03:37 +0100

    Added ranged weapon (wand)
    Players can now switch to a ranged weapon if neccesary

--- Szakdolgozat/Assets/scripts/AttackController.cs
+++ Szakdolgozat/Assets/scripts/AttackController.cs
@@ -10,15 +10,15 @@ public class AttackController : MonoBehaviour
     public PhotonView PV;
 
     private int currentWeaponIndex = 0;
-    public GameObject sword, spear;
-    private void FixedUpdate()
+    public GameObject sword, spear, ranged;
+    private void Update()
     {
         //switch weapon
         Debug.Log(currentWeaponIndex);
         if (Input.GetKeyDown(KeyCode.Q) && !anim[currentWeaponIndex].isPlaying)
         {
             currentWeaponIndex += 1;
-            if (currentWeaponIndex > 1)
+            if (currentWeaponIndex > 2)
             {
                 currentWeaponIndex = 0;
             }
@@ -29,17 +29,17 @@ public class AttackController : MonoBehaviour
                 case Weapons.Sword:
                     sword.SetActive(true);
                     spear.SetActive(false);
-                    //ranged.SetActive(false);
+                    ranged.SetActive(false);
                     break;
                 case Weapons.Spear:
                     sword.SetActive(false);
                     spear.SetActive(true);
-                    //ranged.SetActive(false);
+                    ranged.SetActive(false);
                     break;
                 case Weapons.Ranged:
                     sword.SetActive(false);
                     spear.SetActive(false);
-                    //ranged.SetActive(true);
+                    ranged.SetActive(true);
                     break;
                 default:
                     break;
@@ -51,11 +51,13 @@ public class AttackController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Mouse0) && !anim[currentWeaponIndex].isPlaying)
             {
                 anim[currentWeaponIndex].Play();
-                weaponCollider[currentWeaponIndex].enabled = true;
+                if(currentWeaponIndex != 2)
+                    weaponCollider[currentWeaponIndex].enabled = true;
             }
             if (!anim[currentWeaponIndex].isPlaying)
             {
-                weaponCollider[currentWeaponIndex].enabled = false;
+                if (currentWeaponIndex != 2)
+                    weaponCollider[currentWeaponIndex].enabled = false;
             }
     }
     public enum Weapons { Sword = 0, Spear = 1, Ranged = 2 };
