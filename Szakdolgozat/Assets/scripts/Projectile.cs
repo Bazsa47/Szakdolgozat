f67b12917db282663d10371ddb834fe3addd47e1
Bazsa47 <balazs.wolf@gmail.hu>
2021-02-10 16:59:44 +0100

    Fireball Optimalization
    Fireballs now explode after a given amount of time.

--- Szakdolgozat/Assets/scripts/Projectile.cs
+++ Szakdolgozat/Assets/scripts/Projectile.cs
@@ -1,10 +1,15 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
+public class Projectile : MonoBehaviour, IPunOwnershipCallbacks
 {
     Rigidbody fireball;
+    public float countdown = 3;
+    bool exploded = false;
+    public PhotonView PV;
     void Awake()
     {
         fireball = GetComponent<Rigidbody>();
@@ -12,5 +17,34 @@ public class Projectile : MonoBehaviour
     void Update()
     {
         fireball.velocity = transform.forward * 10;
+        countdown -= Time.deltaTime;
+
+        if (countdown <= 0 && !exploded)
+        {
+            PV.RPC("DestroyFireball",RpcTarget.All,PV.ViewID);
+            exploded = true;
+        }
+    }
+
+    [PunRPC]
+    void DestroyFireball(int viewId)
+    {
+        if (PV.IsMine)
+        {
+            PhotonNetwork.Destroy(PhotonView.Find(viewId).gameObject.transform.parent.gameObject);
+        }
+        else
+        {
+            PV.RequestOwnership();
+        }
+    }
+
+    public void OnOwnershipRequest(PhotonView targetView, Player requestingPlayer)
+    {       
+    }
+
+    public void OnOwnershipTransfered(PhotonView targetView, Player previousOwner)
+    {
+        PhotonNetwork.Destroy(PhotonView.Find(PV.ViewID).gameObject.transform.parent.gameObject);
     }
 }
