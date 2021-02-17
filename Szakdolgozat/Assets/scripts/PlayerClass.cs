d144fced2fd0d0f1e896630837a0ce1765720de2
Bazsa47 <balazs.wolf@gmail.hu>
2021-02-17 14:02:12 +0100

    hp adjustement, added hp timer
    players no longer take double dmg. added a 3 second invulnerabity windows

--- Szakdolgozat/Assets/scripts/PlayerClass.cs
+++ Szakdolgozat/Assets/scripts/PlayerClass.cs
@@ -19,19 +19,40 @@ public class PlayerClass : Entity
         this.PlayerName = PhotonNetwork.LocalPlayer.NickName.ToString();
     }
 
-
+    [SerializeField]
+    private bool canTakeDmg = true;
+    [SerializeField]
+    private float countdown = 3f;
     public override void TakeDmg(float newHp)
     {
         Debug.Log("Dmg taken. HP:" + gameObject.GetComponent<PlayerClass>().Hp);
-        this.GetComponent<PhotonView>().RPC("TakeDmgRPC",RpcTarget.All, this.GetComponent<PhotonView>().ViewID,newHp);
+        this.GetComponent<PhotonView>().RPC("TakeDmgRPC", RpcTarget.All, this.GetComponent<PhotonView>().ViewID, newHp);          
 
+    }
+
+    private void Update()
+    {
+        if (canTakeDmg == false)
+        {
+            Debug.Log("asd");
+            countdown -= Time.deltaTime;
+            if (countdown <= 0f)
+            {
+                canTakeDmg = true;
+                countdown = 3f;
+            }
+        }
     }
 
     [PunRPC]
     public void TakeDmgRPC(int viewID, float newHp)
     {
-        Debug.Log("Rpc called. hp: " + gameObject.GetComponent<PlayerClass>().Hp);
-        PhotonView.Find(viewID).gameObject.GetComponent<PlayerClass>().Hp = newHp;
-        Debug.Log("HP subtracted: " + gameObject.GetComponent<PlayerClass>().Hp);
+        if (canTakeDmg == true)
+        {
+            canTakeDmg = false;
+            Debug.Log("Rpc called. hp: " + gameObject.GetComponent<PlayerClass>().Hp);
+            PhotonView.Find(viewID).gameObject.GetComponent<PlayerClass>().Hp = newHp;
+            Debug.Log("HP subtracted: " + gameObject.GetComponent<PlayerClass>().Hp);
+        }
     }
 }
