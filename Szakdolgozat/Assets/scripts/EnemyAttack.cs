d4b8754919606151fa7053d5251ad93eb2abfe0c
Bazsa47 <balazs.wolf@gmail.hu>
2021-02-20 21:18:52 +0100

    Created enemy attack script
    rotation still needs to be fixed

---
+++ Szakdolgozat/Assets/scripts/EnemyAttack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private float distance;
+    public Animation swordSwing;
+    void Start()
+    {
+       // target = GetComponent<EnemyClass>().Target; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(this.transform.position,target.position) <= distance)
+        {
+            //turn to target
+            transform.rotation = new Quaternion(transform.rotation.x, -target.rotation.y,transform.rotation.z,1);
+
+            //attack
+            if (!swordSwing.isPlaying)
+            {
+                swordSwing.Play();
+            }
+        }
+    }
+}
