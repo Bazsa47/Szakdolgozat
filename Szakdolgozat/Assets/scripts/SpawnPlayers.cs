2dee3b130430706a8abc9a2e56dd40443bed1852
Bazsa47 <balazs.wolf@gmail.hu>
2021-02-26 14:10:13 +0100

    Players now spawn on different spawnpoints

--- Szakdolgozat/Assets/scripts/SpawnPlayers.cs
+++ Szakdolgozat/Assets/scripts/SpawnPlayers.cs
@@ -11,7 +11,16 @@ public class SpawnPlayers : MonoBehaviour
     void Awake() 
     {
         int spawnPoint = Random.Range(0, GameSetup.GS.spawnPoints.Length);
-        PhotonNetwork.Instantiate("PhotonNetworkPlayer", GameSetup.GS.spawnPoints[0].position, GameSetup.GS.spawnPoints[spawnPoint].rotation, 0);
+        int index = 0;
+        for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+        {
+            if (PhotonNetwork.PlayerList[i].IsLocal)
+            {
+                index = i;
+                break;
+            }
+        }
+        PhotonNetwork.Instantiate("PhotonNetworkPlayer", GameSetup.GS.spawnPoints[index].position, GameSetup.GS.spawnPoints[spawnPoint].rotation, 0);
 
         PhotonNetwork.InstantiateSceneObject("Enemy", new Vector3(-6f, 1.831805f, -6.2f), GameSetup.GS.spawnPoints[spawnPoint].rotation, 0);
         PhotonNetwork.InstantiateSceneObject("Enemy", new Vector3(-1f, 1.831805f, -6.2f), GameSetup.GS.spawnPoints[spawnPoint].rotation, 0);
