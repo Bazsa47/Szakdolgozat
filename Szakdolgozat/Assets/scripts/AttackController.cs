eb0a766421a15cf8315ac37151261122f35c5adb
Bazsa47 <balazs.wolf@gmail.hu>
2021-02-26 13:30:35 +0100

    Fireballs are now shot from the wand

--- Szakdolgozat/Assets/scripts/AttackController.cs
+++ Szakdolgozat/Assets/scripts/AttackController.cs
@@ -37,7 +37,7 @@ public class AttackController : MonoBehaviour
                 weaponCollider[currentWeaponIndex].enabled = true;
             else
             {
-                PhotonNetwork.Instantiate("fireball",player.position, this.transform.rotation);
+                PhotonNetwork.Instantiate("fireball",PhotonView.Find(WeaponsPV.ViewID).transform.Find("Wand_").transform.Find("Wand").transform.Find("Weapon-Wand").transform.Find("FireFromHere").transform.position, this.transform.rotation);
             }
         }
 
